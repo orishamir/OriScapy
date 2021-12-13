bb989ce1298ca7f519ccc50cc34cca3fcf0468f7
orishamir <orishamir04@gmail.com>
2021-12-13 21:46:04 +0200

    Importing is now done using `from All import *`

--- Main.py
+++ Main.py
@@ -1,18 +1,8 @@
 from All import *
 
-# https://en.wikipedia.org/wiki/List_of_RFCs
+# Resolve google.com by quering google's DNS server (8.8.8.8)
+pkt = Ether()/IP(dst="8.8.8.8")/UDP(dport=53)/DNS(qd=DNSQR(qname="google.com"))
+res = sendreceive(pkt)
 
-def bytes2bitsPadded(bts):
-    return [f"{bin(bt)[2:]:0>8}" for bt in bts]
-
-def resolve_mac(ip):
-    pkt = Ether()/ARP(pdst=ip)
-    return sendreceive(pkt)[ARP].sender_mac
-
-if __name__ == '__main__':
-    # pkt = Ether()/IP(dst="192.168.1.255")/ICMP()
-    # print(Sendreceive.sendreceive(pkt, flipIP=False))
-    # pkt = Ether()/IP(dst="8.8.8.8")/ICMP()
-    # print(sendreceive(pkt))
-    pkt = Ether()/ARP(pdst="192.168.1.2")
-    print(sendreceive(pkt))
+for answer_record in res.an:
+	print(answer_record.rdata)
