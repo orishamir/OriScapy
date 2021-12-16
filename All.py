e9c8b6dd0c03272a1b6f267dbb064acfe512ad6d
orishamir <orishamir04@gmail.com>
2021-12-16 22:11:53 +0200

    Added to All for easier use

--- All.py
+++ All.py
@@ -1,11 +1,11 @@
-from HelperFuncs import RandShort
 from Sendreceive import sendreceive, send
-from Arp import ARP
-from Ip import IP
+from HelperFuncs import RandShort
+from Dns import DNS, DNSQR, DNSRR
 from Ethernet import Ether
 from Icmp import ICMP
+from Arp import ARP
 from Udp import UDP
-from Dns import DNS, DNSQR, DNSRR
+from Ip import IP
 
 sr = sendreceive
 __all__ = ['sendreceive', 'ARP', 'IP', 'Ether', 'ICMP', 'UDP', 'DNS', 'DNSQR', 'DNSRR', 'RandShort', 'send', 'sr']
